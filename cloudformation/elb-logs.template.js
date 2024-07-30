ca0f2e40e00b4a5f715731f3ce279b8c95f894af
ingalls <nick@ingalls.ca>
2024-07-30 10:45:51 -0600

    Add CHANGELOG & Policy

--- cloudformation/elb-logs.template.js
+++ cloudformation/elb-logs.template.js
@@ -13,7 +13,56 @@ export default cf.merge({
             Type: 'AWS::S3::Bucket',
             Properties: {
                 BucketName: cf.join('-', [cf.stackName, cf.accountId, cf.region]),
+                LifecycleConfiguration: {
+                    Rules: [{
+                        Id: 'MonthlyDelete',
+                        ExpirationInDays: 30,
+                        Status: 'Enabled'
+                    }]
+                }
+            }
+        },
+        LogBucketPolicy: {
+            Type: 'AWS::S3::BucketPolicy',
+            Properties: {
+                Bucket: cf.ref('LogBucket'),
+                PolicyDocument: {
+                    Version: "2012-10-17",
+                    Statement: [
+                        {
+                            Effect: "Allow",
+                            Principal: {
+                                AWS: cf.join(['arn:', cf.partition, ':iam::', cf.findInMap('ELBRegion', cf.region, 'ELBAccount'), ':root'])
+                            },
+                            Action: 's3:PutObject',
+                            Resource: cf.join([cf.getAtt('LogBucket', 'Arn'), '/*'])
+                        }
+                    ]
+                }
             }
         }
     },
+    // This is necessary due to: https://docs.aws.amazon.com/elasticloadbalancing/latest/application/enable-access-logging.html
+    Mappings: {
+        ELBRegion: {
+            "us-gov-east-1": {
+                ELBAccount: '048591011584'
+            },
+            "us-gov-west-1": {
+                ELBAccount: '190560391635'
+            },
+            "us-east-1": {
+                ELBAccount: '127311923021',
+            },
+            "us-east-2": {
+                ELBAccount: '033677994240',
+            },
+            "us-west-1": {
+                ELBAccount: '027434742980',
+            },
+            "us-west-2": {
+                ELBAccount: '797873946194',
+            }
+        }
+    }
 });
